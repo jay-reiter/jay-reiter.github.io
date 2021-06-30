29cd5ddd72cbe6fee03815c0afc2d53b07cb6141
Jay Reiter <jayr2@illinois.edu>
2021-06-29 19:05:26 -0500

    modified nav bar

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import clsx from "clsx";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './NavigationBar.css';
 
+interface NavButtonProps {
+  children: any;
+  tabName: string;
+  currentTab: {
+    name: string;
+    set: (a: string) => void;
+  }
+}
+
+const NavButton: React.FC<NavButtonProps> = ({
+  children,
+  tabName,
+  currentTab: tab
+}) => {
+  return (
+    <div 
+      className={tab.name === tabName ? clsx("NavButton-Selected", "NavButton") : "NavButton"}
+      onClick={() => tab.set(tabName)}
+    >
+      {children}
+    </div>
+  );
+}
 
 function NavigationBar() {
+  const [name, set] = useState("Home");
+  const currentTab = {name, set}
 
-    return (
-        <div className="NavigationBar">
-            <div className="NavButton">
-                <Link to="/index" className="NavLink">Home</Link>
-            </div>
-            <div className="NavButton">
-                <Link to="/TicTacToe" className="NavLink">Play Tic-Tac-Toe</Link>
-            </div>
-            <div className="NavButton">
-                <Link to="/Chess" className="NavLink">Play Chess</Link>
-            </div>
+  return (
+    <div className="NavigationBar">
+      <NavButton tabName={'Home'} currentTab={currentTab}>
+        <Link to="/index" className="NavLink">Home</Link>
+      </NavButton>
+      <NavButton tabName={'Research'} currentTab={currentTab}>
+        <Link to="" className="NavLink">Research</Link>
+      </NavButton>
+      <NavButton tabName={'Projects'} currentTab={currentTab}>
+        <Link to="" className="NavLink">Projects</Link>
+      </NavButton>
+      <NavButton tabName={'Games'} currentTab={currentTab}>
+        Games
+        <div className="DropdownContent">
+          <Link to="/TicTacToe" className="NavLink">Play Tic-Tac-Toe</Link>
+          <Link to="/Chess" className="NavLink">Play Chess</Link>
         </div>
-    );
+      </NavButton>
+    </div>
+  );
 }
 
 export default NavigationBar;
---
+++ src/PageHeader.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+interface Props {
+
+}
+
+export const PageHeader = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
