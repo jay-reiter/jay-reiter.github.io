43d71663a5ef731d93619e47161bfc9823ad8f9a
Jay Reiter <reitjay@gmail.com>
2026-04-27 12:58:33 -0700

    jackson abstract

--- src/assets/dubtop-talks/spring2026.tsx
+++ src/assets/dubtop-talks/spring2026.tsx
@@ -18,7 +18,7 @@ const talks: ReactElement[] = [
         foundational development in motivic homotopy theory. As a consequence,
         it also provides a key step toward the Lichtenbaum--Quillen conjecture.
         In this talk, I will begin by introducing étale cohomology and then
-        present Voevodsky’s norm residue theorem. I will briefly explain its
+        present Voevodsky's norm residue theorem. I will briefly explain its
         relationship to the Lichtenbaum--Quillen conjecture. As an application,
         I will discuss the computation of the motivic cohomology of a point over
         real closed fields and present a small recent result of my own in this
@@ -31,21 +31,57 @@ const talks: ReactElement[] = [
     speaker="Ting Gong"
     title="A formal talk"
     date="April 24"
-    abstract={<>In this talk, I will introduce formal groups and explain some of the reasons they are important in algebraic geometry, topology, number theory, and representation theory. I will discuss their role in the origins of chromatic homotopy theory, and also say something about Artin--Mazur formal groups from algebraic geometry. If time permits, I will briefly mention topological modular forms.</>}
+    abstract={
+      <>
+        In this talk, I will introduce formal groups and explain some of the
+        reasons they are important in algebraic geometry, topology, number
+        theory, and representation theory. I will discuss their role in the
+        origins of chromatic homotopy theory, and also say something about
+        Artin--Mazur formal groups from algebraic geometry. If time permits, I
+        will briefly mention topological modular forms.
+      </>
+    }
     speakerWebsite="https://sites.google.com/view/tinggong"
   />,
   <DubTopTalkItem
     speaker="Jackson Morris"
-    title="TBA"
-    date="May 1"
-    abstract={<>TBA</>}
-    speakerWebsite="https://jackson-morris.github.io/"
-  />,
-  <DubTopTalkItem
-    speaker="Jackson Morris"
-    title="TBA"
-    date="May 8"
-    abstract={<>TBA</>}
+    title={
+      <>
+        Higher real <Math tex={String.raw`K`} />
+        -theories, redshift, and blueshift
+      </>
+    }
+    date="May 1 & May 8"
+    abstract={
+      <>
+        The <Math tex={String.raw`K(n)`} />
+        -local sphere is a fundamental object of interest in chromatic homotopy
+        theory. One may access this object by Galois descent from its separable
+        closure, the Lubin-Tate spectrum of height <Math
+          tex={String.raw`n`}
+        />{" "}
+        attached to the Honda formal group law. However, as the Galois group{" "}
+        <Math tex={String.raw`\mathbb{G}_n`} /> is a large{" "}
+        <Math tex={String.raw`p`} />
+        -adic Lie group, this is not very practical. An easier task is to
+        approximate the <Math tex={String.raw`K(n)`} />
+        -local sphere by descent along finite subgroups of{" "}
+        <Math tex={String.raw`\mathbb{G}_n`} />. The resulting homotopy fixed
+        points are known as higher real <Math tex={String.raw`K`} />
+        -theories, and they are among the most important and well-studied
+        spectra we have at our disposal.
+        <br />
+        In this series of talks I will discuss the role that higher real{" "}
+        <Math tex={String.raw`K`} />
+        -theories play in chromatic homotopy theory. In talk 1, I will introduce
+        the main characters and landscape and mention tools for computation. In
+        talk 2, I will introduce redshift in algebraic{" "}
+        <Math tex={String.raw`K`} />
+        -theory and blueshift in Tate fixed points for higher real{" "}
+        <Math tex={String.raw`K`} />
+        -theories.
+      </>
+    }
     speakerWebsite="https://jackson-morris.github.io/"
   />,
   <DubTopTalkItem
